3f3b3eb59ce36aeb69e16e1c01a5309ae1b8c452
lsimp <lsimp@qq.com>
2024-01-29 16:43:24 +0800

    we get the new mode of

--- cat.py
+++ cat.py
@@ -2,12 +2,17 @@ import sys
 
 def cat():
     print('Meow!')
+def dog():
+    print('Woof!')
 def default():
     print('Hello',sys.argv[1])
 
 def main():
     if sys.argv[1] == 'cat': 
         cat()
+    else if:
+        sys.argv[1] == 'dag'：
+        dog()
     else:
         default()
 if __name__ == '__main__':
